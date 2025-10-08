35c597b6dee7f322b85c3f3fcc956120618ed3b9
ZDB <rafael05garlant@gmail.com>
2025-10-08 13:45:10 -0300

    Criando o index.php

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'Controller/UserController.php'; 
+
+$controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Home';
+$controllerClass = $controllerName . 'Controller';
+
+$actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+if(!class_exists($controllerClass)){
+  http_response_code(404);
+  die("Controller '{$controllerClass}' não encontrado.");
+}
+
+$controller = new $controllerClass();
+
+if(!method_exists($controller, $actionName)){
+  http_response_code(404);
+  die("Ação '{$actionName}' não encontrada no Controller.");
+}
+
+
+$controller->$actionName();
