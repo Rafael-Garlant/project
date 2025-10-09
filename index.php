0d0f0034861c44097be6682c58983b42fa6aba3d
Rafael Garlant <rafael05garlant@gmail.com>
2025-10-08 21:34:04 -0300

    Adicionando arquivos backend

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+ini_set('display_errors', 1);s
+error_reporting(E_ALL);
+
+require_once 'patientController.php';
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-<<<<<<< HEAD
-require_once 'patientController.php';
-=======
 require_once 'Controller/UserController.php'; 
 
 $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'Home';
@@ -19,11 +21,10 @@ if(!class_exists($controllerClass)){
 
 $controller = new $controllerClass();
 
-if(!method_exists($controller, $actionName)){
+if(!method_exists($controller, $actionName))
   http_response_code(404);
   die("Ação '{$actionName}' não encontrada no Controller.");
 }
 
 
 $controller->$actionName();
->>>>>>> 35c597b6dee7f322b85c3f3fcc956120618ed3b9
